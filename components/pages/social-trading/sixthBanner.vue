05fe8f9b04611c504f8c7f8f806c6efd6223f914
Artloveyou <hiartloveyou@gmail.com>
2020-04-14 19:36:49 +0300

    mini cards

--- components/pages/social-trading/sixthBanner.vue
+++ components/pages/social-trading/sixthBanner.vue
@@ -20,34 +20,41 @@
       </v-col>
     </v-row>
     <v-row>
-      <v-col class="col-12 col-sm-3">
+
+      <v-col class="col-12 col-md-3">
         <div class="mini-card maximum">
           <img src="~/assets/img/iconMaximum.svg"/>
-          <p>Максимальная относительная просадка</p>
+          <div class="mini-card-description">Максимальная относительная просадка</div>
         </div>
-
       </v-col>
-      <v-col class="col-12 col-sm-3">
 
+      <v-col class="col-12 col-md-3">
+        <div class="mini-card deposit">
+          <img src="~/assets/img/iconDeposit.svg"/>
+          <div class="mini-card-description">Максимальная загрузка депозита</div>
+        </div>
       </v-col>
-      <v-col class="col-12 col-sm-3">
 
+      <v-col class="col-12 col-md-3">
+        <div class="mini-card clock">
+          <img src="~/assets/img/iconClock.svg"/>
+          <div class="mini-card-description">Срок жизни торгового счёта</div>
+        </div>
       </v-col>
-      <v-col class="col-12 col-sm-3">
 
+      <v-col class="col-12 col-md-3">
+        <div class="mini-card scale">
+          <img src="~/assets/img/iconScale.svg"/>
+          <div class="mini-card-description">Размер кредитного плеча</div>
+        </div>
       </v-col>
+
     </v-row>
   </div>
 </template>
 
 <script>
-  import iconMaximum from '~/assets/img/iconMaximum.vue'
-
-  export default {
-    components: {
-      iconMaximum
-    }
-  }
+  export default {}
 </script>
 
 <style>
@@ -69,13 +76,24 @@
     align-content: stretch;
     border: 2px solid;
     border-radius: 5px 5px;
+    padding: 10px 10px;
+    height: 80px;
+  }
+
+  .mini-card-description {
+    margin: 0 0 0 15px;
   }
 
   .maximum {
     border-color: #F8E49E;
   }
-
-  .maximum svg {
-    fill: #F8E49E;
+  .deposit {
+    border-color: #9BAFE2;
+  }
+  .clock {
+    border-color: #A4EED0;
+  }
+  .scale{
+    border-color: #F5C1E8;
   }
 </style>
