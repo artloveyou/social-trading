eae54a34c508e08df2ef28dc2311cfce21796e1b
Artloveyou <hiartloveyou@gmail.com>
2020-04-15 14:05:50 +0300

    add icons

---
+++ assets/icons/iconMaximum.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg width="32" height="27" viewBox="0 0 32 27">
+    <path d="M1 1C6.83333 1 8.5 26 16 26C23.5 26 25.1667 1 31 1" stroke="#F8E49E" stroke-width="2" stroke-linecap="round"/>
+  </svg>
+</template>
---
+++ assets/icons/iconScale.vue
@@ -0,0 +1,7 @@
+<template>
+  <svg width="30" height="30" viewBox="0 0 30 30">
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M28 2H2V28H28V2ZM0 0V30H30V0H0Z" fill="#F5C1E8"/>
+    <path d="M19 19H30V30H19V19Z" fill="#F5C1E8"/>
+  </svg>
+</template>
+
