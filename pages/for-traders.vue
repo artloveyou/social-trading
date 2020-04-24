a54d4ca7f6f64689c0d7a4716182983752ea6a64
Artloveyou <hiartloveyou@gmail.com>
2020-04-24 13:44:20 +0300

    deploy

--- pages/for-traders.vue
+++ pages/for-traders.vue
@@ -1,7 +1,43 @@
 <template>
-  <v-layout>
-    <v-flex class="text-center">
-      For Traders
-    </v-flex>
-  </v-layout>
+  <component :is="pageType" />
 </template>
+
+<script>
+  import pageFull from '~/components/pages/social-trading/pageFull.vue'
+  import pageMobile from '~/components/pages/social-trading/pageMobile.vue'
+
+  export default {
+    components: {
+      pageFull, pageMobile
+    },
+    data(){
+      return {
+        width: 600,
+        window: {
+          width: 0
+        }
+      }
+    },
+    created() {
+      window.addEventListener('resize', this.handleResize);
+      this.handleResize();
+    },
+    destroyed() {
+      window.removeEventListener('resize', this.handleResize);
+    },
+    methods: {
+      handleResize() {
+        this.window.width = window.innerWidth;
+      },
+    },
+    computed: {
+      pageType: function () {
+        return this.window.width > this.width ? 'pageFull' : 'pageMobile';
+      }
+    }
+  }
+</script>
+
+
+
+
